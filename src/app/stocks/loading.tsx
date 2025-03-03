06c28d1ed99d25e3e71371b4e96c978342313640
Ammar Alhasan <ammar.alhasaan@gmail.com>
2025-03-03 23:11:02 +0400

    add app loading

---
+++ src/app/stocks/loading.tsx
@@ -0,0 +1,23 @@
+import {Skeleton} from "@/components/ui";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <>
+      <Skeleton className="rounded-lg">
+        <div className="h-24 rounded-lg bg-default-300"/>
+      </Skeleton>
+      <div className="space-y-3">
+        <Skeleton className="w-3/5 rounded-lg">
+          <div className="h-3 w-3/5 rounded-lg bg-default-200"/>
+        </Skeleton>
+        <Skeleton className="w-4/5 rounded-lg">
+          <div className="h-3 w-4/5 rounded-lg bg-default-200"/>
+        </Skeleton>
+        <Skeleton className="w-2/5 rounded-lg">
+          <div className="h-3 w-2/5 rounded-lg bg-default-300"/>
+        </Skeleton>
+      </div>
+    </>
+  );
+}
