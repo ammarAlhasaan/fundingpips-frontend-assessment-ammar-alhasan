7e7681b43cbe041d33754ff5c0affb65c9530142
Ammar Alhasan <ammar.alhasaan@gmail.com>
2025-03-03 23:05:58 +0400

    add app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import {Providers} from "@/app/providers";
 import {Navbar} from "@/components";
+
+import "./globals.css";
+import {Suspense} from "@/components/ui";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Suspense fallback={<p>Loading...</p>}>
       <Providers>
         <Navbar />
         {children}
       </Providers>
+      </Suspense>
       </body>
     </html>
   );
